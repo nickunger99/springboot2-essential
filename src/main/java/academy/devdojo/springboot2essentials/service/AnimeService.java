513cb1a4f703f0bb9c6aa436e975fe3c3a6418c4
nickunger99 <nicolas.unger@yahoo.com.br>
2023-04-26 14:43:29 -0300

    aula 9 : método post

--- src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
@@ -6,13 +6,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
 //    private final AnimeRepository animeRepository;
 
-    private List<Anime> animes = List.of(new Anime(1L, "Boku no Hero"), new Anime(2L, "Berserk"));
+    private static List<Anime> animes;
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "Boku no Hero"), new Anime(2L, "Berserk")));
+    }
 
     public List<Anime> ListAll() {
         return animes;
@@ -23,5 +28,11 @@ public class AnimeService {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+    }
+
+    public Anime save(Anime anime) {
+    anime.setId(ThreadLocalRandom.current().nextLong(3, 100000));
+    animes.add(anime);
+    return anime;
     }
 }
