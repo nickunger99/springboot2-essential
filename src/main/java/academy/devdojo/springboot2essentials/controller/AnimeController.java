c49acd5a6a985f01e219956d09c1ecbad7292883
nickunger99 <nicolas.unger@yahoo.com.br>
2023-04-26 13:06:58 -0300

    aula 6 e 7 - get anime/list

---
+++ src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
@@ -0,0 +1,28 @@
+package academy.devdojo.springboot2essentials.controller;
+
+import academy.devdojo.springboot2essentials.domain.Anime;
+import academy.devdojo.springboot2essentials.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
+public class AnimeController {
+
+    private final DateUtil dateUtil;
+
+    //localhost:8080/anime/list
+    @GetMapping(path = "list")
+    public List<Anime> list(){
+        log.info(dateUtil.formatLocalDatwTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("Boku no Hero"), new Anime("Berserk"));
+    }
+}
