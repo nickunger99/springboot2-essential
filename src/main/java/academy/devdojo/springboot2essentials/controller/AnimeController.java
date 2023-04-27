ab9b2dc9b3dff6e5de43b1ad9ae9a95f90d7e014
nickunger99 <nicolas.unger@yahoo.com.br>
2023-04-27 10:38:53 -0300

    aula 11 : metodo put

--- src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
@@ -43,4 +43,11 @@ public class AnimeController {
         animeService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
+    @PutMapping
+    public ResponseEntity<Anime> replace(@RequestBody Anime anime) {
+        animeService.replace(anime);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
