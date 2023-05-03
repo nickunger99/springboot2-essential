4eede47bd9916399de455d9355167a55c0e002cb
nickunger99 <nicolas.unger@yahoo.com.br>
2023-05-03 13:25:54 -0300

    20 - Validação de campos

--- src/main/java/academy/devdojo/springboot2essentials/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2essentials/domain/Anime.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
@@ -19,6 +16,8 @@ public class Anime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
     private String name;
 
 
--- src/main/java/academy/devdojo/springboot2essentials/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2essentials/requests/AnimePostRequestBody.java
@@ -2,7 +2,12 @@ package academy.devdojo.springboot2essentials.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class AnimePostRequestBody {
+
+    @NotEmpty(message = "The anime name cannot be empty")
+   // @NotNull(message = "The anime name cannot be null")
     private String name;
 }
