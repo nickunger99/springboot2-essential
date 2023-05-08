162d1efe145e8bf402d3f9e5b6f77a44f4efee28
nickunger99 <nicolas.unger@yahoo.com.br>
2023-05-08 12:35:02 -0300

    29 - RestTemplate PUT and DELETE

--- src/main/java/academy/devdojo/springboot2essentials/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2essentials/client/SpringClient.java
@@ -37,9 +37,21 @@ public class SpringClient {
         ResponseEntity<Anime> samuraiChamplooSaved = new RestTemplate().exchange("http://localhost:8080/animes/", HttpMethod.POST,
                 new HttpEntity<>(samuraiChamploo, createJsonHeader()), Anime.class);
         log.info("saved anime {}", samuraiChamplooSaved);
+
+        Anime animeToBeUpdated = samuraiChamplooSaved.getBody();
+        animeToBeUpdated.setName("Samurai Champloo 2");
+
+        ResponseEntity<Void> samuraiChamplooUpdated = new RestTemplate().exchange("http://localhost:8080/animes/", HttpMethod.PUT,
+                new HttpEntity<>(animeToBeUpdated, createJsonHeader()), Void.class);
+        log.info(samuraiChamplooUpdated);
+
+        ResponseEntity<Void> samuraiChamplooDeleted = new RestTemplate().exchange("http://localhost:8080/animes/{id}", HttpMethod.DELETE,
+                null, Void.class, animeToBeUpdated.getId());
+        log.info(samuraiChamplooDeleted);
+
     }
 
-    private static HttpHeaders createJsonHeader(){
+    private static HttpHeaders createJsonHeader() {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         return httpHeaders;
